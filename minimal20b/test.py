0910b9c9396d48ff44e47a367eefbcc319f944dc
Feanorz <meselfme6@gmail.com>
2022-04-21 21:52:07 +0100

    Weight Preprocessing

--- minimal20b/test.py
+++ minimal20b/test.py
@@ -1,36 +1,15 @@
 import torch.nn as nn
 import torch
+device = torch.device("cuda:0")
 import time
 
-# class Test(nn.Module):
-#     def __init__(self, i):
-#         super().__init__()
-#         self.param1 = torch.nn.parameter.Parameter(torch.tensor([1., 2., 3.], dtype=torch.float32))
-#         self.tensor1 = torch.tensor([1., 2., 3.], dtype=torch.float32)
-#
-#     def forward(self, x):
-#         return x + self.param1
-#
-#
-# test_class = Test(1)
-#
-# states = test_class.state_dict()
-# print(states, list(test_class.parameters()))
-# def chunk(x, n):
-#     length = len(x)
 
-# Multiprocessing init of transformer layers
-self.layer_list = nn.ModuleList([])
-with Pool(4) as p:
-    layer_inputs = [(args, use_cache) for _ in range(args.num_layers)]
+print("start")
+print(1)
+x = torch.ones(5000000, dtype=torch.float16, device=device)
 
-    for i in range(0, args.num_layers, 4):
-        batch = layer_inputs[i:i + 4]
-        transformer_layers = p.starmap(TransformerLayer, layer_inputs)
 
-        for transformer_layer in transformer_layers:
-            self.layer_list.append(transformer_layer)
-print("All layers initialised")
-
+time.sleep(10)
+print(x)
 
 
